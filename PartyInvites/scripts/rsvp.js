b0bd35312cb651a3e1801f773ffd88af17edb48f
LUIS-PC\Luis <ldurazo@mimoni.com>
2016-09-01 12:04:11 -0500

    New project for web api

---
+++ PartyInvites/scripts/rsvp.js
@@ -0,0 +1,49 @@
+﻿var model = {
+    view: ko.observable("welcome"),
+    rsvp : {
+        name: ko.observable(""),
+        email: "",
+        willattend: ko.observable("true")
+    },
+    attendes: ko.observableArray([])
+}
+
+var showForm = function()
+{
+    model.view("form");
+}
+
+var sendRsvp = function() {
+    $.ajax("/api/rsvp", {
+        type: "POST",
+        data: {
+            name: model.rsvp.name(),
+            email: model.rsvp.email,
+            willattend: model.rsvp.willattend()
+        },
+
+        success: function() {
+            getAttendes();
+        }
+    });
+}
+
+var getAttendes = function() {
+        $.ajax("/api/rsvp",
+        {
+            type: "GET",
+            success: function(data) {
+                model.attendes.removeAll();
+                model.attendes.push.apply(model.attendes, data.map(function(rsvp) {
+                    return rsvp.Name;
+                }));
+
+                model.view("thanks");
+            }
+        });
+}
+
+$(document).ready(function() {
+    ko.applyBindings();
+});
+ 
---
+++ Primer/Controllers/PageSizeController.cs
@@ -0,0 +1,21 @@
+﻿ 
+using System.Diagnostics; 
+using System.Net; 
+using System.Web.Http;
+
+namespace Primer.Controllers
+{
+    public class PageSizeController : ApiController
+    {
+        private static string TargetUrl = "http://apress.com";
+
+        public long GetPageSize()
+        {
+            WebClient wc = new WebClient();
+            Stopwatch sw = Stopwatch.StartNew();
+            byte[] apressData = wc.DownloadData(TargetUrl);
+            Debug.WriteLine("Elapsed ms: {0}", sw.ElapsedMilliseconds);
+            return apressData.LongLength;
+        }
+    }
+}
